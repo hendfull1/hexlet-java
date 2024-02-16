0ccdef510d12bd42e728e732748b741c1696e4ed
hendfull1 <iigorvakhrushev@gmail.com>
2024-02-16 21:16:25 +0600

    update

--- io/hexlet/One.java
+++ io/hexlet/One.java
@@ -1,7 +1,11 @@
 package io.hexlet;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class One { 
 	public static void main(String[] args) {
-		Two.greeting();
+		var hexlet = "hexlet";
+		var reversed = StringUtils.reverse(hexlet);
+		System.out.println(reversed);
        	}
 }
